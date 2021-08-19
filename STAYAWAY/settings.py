34e1a872b661d5dd5058376963952e1ec5313f33
harshithsuresh <34330677+harshithsuresh@users.noreply.github.com>
2021-08-19 23:49:33 +0530

    Removed secret Key

--- STAYAWAY/settings.py
+++ STAYAWAY/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4jf34vs7ad@4r-jl9o@2_0yoa+!3@#bddmim*)@ko%d(d5c5sb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
